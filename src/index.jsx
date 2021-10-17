6c512c1f01f7fe821dc119c7a63210194bfa8b16
siradheek <siradhee_k@hotmail.com>
2021-10-17 16:43:36 +0700

    remove serviceWorker

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import {Provider} from 'react-redux';
 import store from './store/store.js';
 
 const serviceWorker = require('./serviceWorker')
-serviceWorker.register();
+serviceWorker.unregister();
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
