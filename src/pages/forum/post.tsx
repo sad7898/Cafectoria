c37ecf0a010da5f1a1cc95c1ac4c7ea811ba4e8d
siradheek <siradhee_k@hotmail.com>
2021-11-07 02:25:37 +0700

    fix isAuthor logic

--- src/pages/forum/post.tsx
+++ src/pages/forum/post.tsx
@@ -1,74 +1,69 @@
-import React, { useState, useEffect } from "react";
-import { Wrapper } from "../../components/containers";
-import { Header } from "../../components/utilities";
-import { useParams, useHistory } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import Axios from "axios";
-import useLoading from "../../contexts/loadingContext";
+import React, { useState, useEffect } from "react"
+import { Wrapper } from "../../components/containers"
+import { Header } from "../../components/utilities"
+import { useParams, useHistory } from "react-router-dom"
+import { Button } from "react-bootstrap"
+import Axios from "axios"
+import useLoading from "../../contexts/loadingContext"
+import { useSelector } from "react-redux"
+import { RootState } from "../../store/store"
 export interface PostProps {
-  id?: string;
-  topic: string;
-  tags: string[];
-  author?: string;
+  id?: string
+  topic: string
+  tags: string[]
+  author?: string
 }
 interface PostDataProps {
-  topic: string;
-  text: string;
+  topic: string
+  text: string
 }
 interface PostDataResponse extends PostDataProps {
-  isAuthor: boolean;
+  author: string
 }
 const Post = () => {
-  const { setLoading } = useLoading();
-  const [isAuthor, setAuthor] = useState(false);
-  const history = useHistory();
-  const [postData, setPostData] = useState<PostDataProps>();
+  const { setLoading } = useLoading()
+  const [isAuthor, setAuthor] = useState(false)
+  const history = useHistory()
+  const [postData, setPostData] = useState<PostDataProps>()
+  const { user } = useSelector((state: RootState) => state.auth)
 
-  const { id } = useParams<{ id: string }>();
+  const { id } = useParams<{ id: string }>()
   const handleDel = async () => {
-    setLoading(true);
+    setLoading(true)
     await Axios.delete(`https://cafetoria-backend.herokuapp.com/post/${id}`, {
       withCredentials: true,
     })
       .then((res) => {
-        history.push("/");
+        history.push("/")
       })
       .catch((err) => {
-        alert("You cannot delete post if you are not the author");
-        history.push("/");
-      });
-    setLoading(false);
-  };
+        alert("You cannot delete post if you are not the author")
+        history.push("/")
+      })
+    setLoading(false)
+  }
   useEffect(() => {
     async function loadContent() {
-      setLoading(true);
-      await Axios.get<PostDataResponse[]>(
-        `https://cafetoria-backend.herokuapp.com/post/${id}`,
-        {
-          withCredentials: true,
-        }
-      ).then((res) => {
-        if (res.data[0].isAuthor) {
-          setAuthor(true);
+      setLoading(true)
+      await Axios.get<PostDataResponse[]>(`https://cafetoria-backend.herokuapp.com/post/${id}`, {
+        withCredentials: true,
+      }).then((res) => {
+        if (res.data[0].author === user) {
+          setAuthor(true)
         } else {
-          setAuthor(false);
+          setAuthor(false)
         }
         setPostData({
           topic: res.data[0].topic,
           text: res.data[0].text,
-        });
-      });
-      setLoading(false);
+        })
+      })
+      setLoading(false)
     }
-    loadContent();
-  }, [id, setLoading]);
+    loadContent()
+  }, [id, setLoading, user])
   return (
-    <Wrapper
-      bg="#dedede"
-      rborder="10px"
-      pd="1rem 1rem 1rem 1rem"
-      mg="1rem auto auto auto"
-    >
+    <Wrapper bg="#dedede" rborder="10px" pd="1rem 1rem 1rem 1rem" mg="1rem auto auto auto">
       {isAuthor ? (
         <div className="d-flex flex-row justify-content-end">
           <Button variant="danger" onClick={handleDel}>
@@ -81,6 +76,6 @@ const Post = () => {
       <Header>{postData?.topic}</Header>
       <Wrapper>{postData?.text}</Wrapper>
     </Wrapper>
-  );
-};
-export default Post;
+  )
+}
+export default Post
