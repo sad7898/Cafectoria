75b4a57b8541738c5cb46e2943b60f4367fe1c8d
siradheek <siradhee_k@hotmail.com>
2022-09-25 20:57:25 +0200

    add dockerfile & dockerignore

--- src/components/guards/withAuthGuard.tsx
+++ src/components/guards/withAuthGuard.tsx
@@ -7,7 +7,7 @@ const LoadingAuth: React.FC<any> = () => <div>Loading...</div>
 const withAuthGuard = <T,>(Component: ComponentType<T>, redirectTo?: string): React.FC<T> => {
   return ({ ...props }) => {
     const auth = useSelector((state: RootState) => state.auth)
-    if (auth.status === AuthStatus.AUTH) return <Component {...(props as T)} />
+    if (auth.status === AuthStatus.AUTH) return <Component {...(props as T & JSX.IntrinsicAttributes)} />
     else if (auth.status === AuthStatus.PENDING) return null
     return redirectTo ? <Navigate replace to={redirectTo}></Navigate> : null
   }
