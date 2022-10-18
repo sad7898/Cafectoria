fa18d739f96b14b5c08fbdf1b4ff289dd0f77736
siradheek <siradhee_k@hotmail.com>
2022-10-18 15:08:11 +0200

    fix responsiveness

--- src/components/containers.tsx
+++ src/components/containers.tsx
@@ -1,32 +1,32 @@
-import { Container } from "react-bootstrap";
-import styled from "styled-components";
+import { Container } from "react-bootstrap"
+import styled from "styled-components"
 interface ContainerProps {
-  minh?: string;
-  maxh?: string;
-  maxw?: string;
-  minw?: string;
-  bg?: string;
-  mg?: string;
-  pd?: string;
+  minh?: string
+  maxh?: string
+  maxw?: string
+  minw?: string
+  bg?: string
+  mg?: string
+  pd?: string
 }
 interface BootStrapContainerProps extends ContainerProps {
-  border?: string;
-  rborder?: string;
-  bordertop?: string;
-  borderbot?: string;
-  borderleft?: string;
+  border?: string
+  rborder?: string
+  bordertop?: string
+  borderbot?: string
+  borderleft?: string
 }
 interface WrapperProps extends ContainerProps {
-  width?: string;
-  border?: string;
-  rborder?: string;
-  shadow?: string;
+  width?: string
+  border?: string
+  rborder?: string
+  shadow?: string
 }
 interface DecoratedBlockProps extends ContainerProps {
-  width?: string;
-  border?: string;
-  rborder?: string;
-  shadow?: boolean;
+  width?: string
+  border?: string
+  rborder?: string
+  shadow?: boolean
 }
 export const BSWrapper = styled(Container)<BootStrapContainerProps>`
     width: 100%;
@@ -45,9 +45,9 @@ export const BSWrapper = styled(Container)<BootStrapContainerProps>`
     border-top : ${(props) => props.bordertop};
     border-bottom : ${(props) => props.borderbot};
     border-left : ${(props) => props.borderleft};
-`;
+`
 export const Wrapper = styled.div<WrapperProps>`
-    width: ${(props) => (props.width ? props.width : "100%")}
+    width: ${(props) => (props.width ? props.width : "100%")};
     height: auto;
     min-height: ${(props) => props.minh}px;
     max-height: ${(props) => props.maxh}px;
@@ -59,7 +59,7 @@ export const Wrapper = styled.div<WrapperProps>`
     border : ${(props) => props.border};
     border-radius: ${(props) => props.rborder};
     box-shadow: ${(props) => props.shadow};
-`;
+`
 export const DecoratedBlock = styled.div<DecoratedBlockProps>`
     width: 100%;
     height: auto;
@@ -69,11 +69,9 @@ export const DecoratedBlock = styled.div<DecoratedBlockProps>`
     min-wdith ${(props) => props.minw}px;
     background: ${(props) => (props.bg ? props.bg : "var(--grey-color)")};
     border-radius: 10px;
-    box-shadow: ${(props) =>
-      props.shadow ? "10px 10px 5px rgba(71,71,71,0.5)" : "none"};
-    padding: ${(props) =>
-      props.pd ? props.pd : "1.5rem 1.5rem 1.5rem 1.5rem"};   
-`;
+    box-shadow: ${(props) => (props.shadow ? "10px 10px 5px rgba(71,71,71,0.5)" : "none")};
+    padding: ${(props) => (props.pd ? props.pd : "1.5rem 1.5rem 1.5rem 1.5rem")};   
+`
 
 export const EyeLevel = styled.div`
   width: 100%;
@@ -90,4 +88,12 @@ export const EyeLevel = styled.div`
     padding-top: 6%;
     padding-bottom: 6%;
   }
-`;
+`
+
+export const DynamicBlock = styled(DecoratedBlock)`
+  @media (max-width: 768px) {
+    background: transparent;
+    padding: 0 0 0 0;
+    box-shadow: 0 0 0;
+  }
+`
